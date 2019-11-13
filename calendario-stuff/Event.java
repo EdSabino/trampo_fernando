b5f886f2a086d0b2d441077a37f757a6b35ecd5e
aluno-uffs <aluno-uffs@405host28.cco.uffs.edu.br>
2019-11-13 19:51:51 -0300

    am pm

--- calendario-stuff/Event.java
+++ calendario-stuff/Event.java
@@ -1,3 +1,11 @@
 class Event {
     public int day, month, year, hour, minute;
+
+    public String partOfTheDay() {
+        if (hour > 12) {
+            return 'PM';
+        } else {
+            return 'AM';
+        }
+    }
 }
