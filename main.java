08f3f3368385cce98e658ae2ae1576195657e585
Gabriel Guerezi <42014074+guerezi@users.noreply.github.com>
2019-11-13 20:46:21 -0300

    Update main.java

--- main.java
+++ main.java
@@ -6,26 +6,27 @@ import java.util.Scanner;
 public class Main{
     public static void main(String[] args) {
         
-        int maior = 0;
-        int menor = 999999999;
-        double media =0;
+        int maior, menor;
+        double media;
         List<Double> ints = new ArrayList<>(Arrays.asList());
         Scanner sc = new Scanner(System.in); 
         
-        //while(sc.hasNext()){
-        //   ints.add(Integer.valueOf(sc.next()));
-        //}
-        ints.add(1.0);
-        ints.add(2.0);
-        ints.add(3.0);
+        while(sc.hasNextLine()){
+           ints.add(Integer.valueOf(sc.next()));
+        }
+
         sc.close();
 
         for ( int i = 0; i<ints.size(); i++) {
-            if(ints.get(i) % 7 && ints.get(i) % 10)
+            if(ints.get(i)%7 && ints.get(i)%10)
               System.out.print(ints.get(i));
         }
 
-        for ( int i = 0; i<ints.size(); i++) {
+        maior = ints.get(0);
+        menor = ints.get(0);
+        media = ints.gets(0);
+
+        for ( int i = 1; i<ints.size(); i++) {
             if(ints.get(i)> maior) maior = ints.get(i);
             if(ints.get(i)< menor) menor = ints.get(i);
             media+=ints.get(i);
