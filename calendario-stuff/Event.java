c4a0da7cf59535e1ae6ca73dd3e30101bfbf77cc
Everaldo Gomes <everaldo332@gmail.com>
2019-11-13 20:02:34 -0300

    Update Event.java

--- calendario-stuff/Event.java
+++ calendario-stuff/Event.java
@@ -5,4 +5,14 @@ class Event {
     public void setIsAm(String part) {
         isAm = part == "AM";
     }
+    
+     public int setDia(int d){
+	if(d <= 0 || d >= 31) return 0;
+	this.day = d;
+    }
+
+    public int setMes(int m){
+	if(m <= 0 || m >= 12) return 0;
+	this.month = m;
+    }
 }
