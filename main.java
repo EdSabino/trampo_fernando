d2df1e01e3dbebcd39e393e69baac881c37300a0
leobsilva <31291693+leobsilva@users.noreply.github.com>
2019-11-13 20:46:53 -0300

    Update main.java

--- main.java
+++ main.java
@@ -16,7 +16,25 @@ public class Main{
         }
 
         sc.close();
+        for ( int i = 0; i<ints.size(); i++) {
+            System.out.println(ints.get(i));
+        }
+        int cont;
+        int num = 0;
 
+        for(Integer i : ints ){
+            cont = 0;
+
+            for(int u=1; u<=i;u++){
+                if(i%u == 0){
+                    cont++;
+                    num = u;
+                }
+            }
+            if(cont == 2) {
+                System.out.println("O Número: " + num + " é primo e seus primos são: 1 e "+ num);
+            }
+        }
         for ( int i = 0; i<ints.size(); i++) {
             if(ints.get(i)%7 && ints.get(i)%10)
               System.out.print(ints.get(i));
