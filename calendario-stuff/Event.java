c3f478be6ca5590dd239da145991be7d97c1b0ed
aluno-uffs <aluno-uffs@405host28.cco.uffs.edu.br>
2019-11-13 20:38:24 -0300

    difference

--- calendario-stuff/Event.java
+++ calendario-stuff/Event.java
@@ -1,3 +1,6 @@
+import java.time.LocalDateTime;
+import java.time.Duration;
+
 class Event {
     public int day, month, year, hour, minute;
     public boolean isAm;
@@ -6,13 +9,21 @@ class Event {
         isAm = part == "AM";
     }
     
-     public int setDia(int d){
-	if(d <= 0 || d >= 31) return 0;
-	this.day = d;
+    public int setDia(int d){
+        if(d <= 0 || d >= 31) return 0;
+        this.day = d;
     }
 
     public int setMes(int m){
-	if(m <= 0 || m >= 12) return 0;
-	this.month = m;
+        if(m <= 0 || m >= 12) return 0;
+        this.month = m;
+    }
+
+    public LocalDateTime asLocalDateTime() {
+        return LocalDateTime.of(year, month, day, hour, minute);
+    }
+
+    public int difference(Event otherEvent) {
+        return Duration.between(this.asLocalDateTime(), otherEvent.asLocalDateTime()).getSeconds();
     }
 }
